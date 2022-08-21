b7424f17be1ba2c423a7b6c82d06ad9893314fd5
brtsrm <bsarmis@outlook.com>
2022-08-22 00:04:34 +0300

    if elseif else kullanımı

---
+++ ders2/if.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+	/*
+		a := 10
+		b := 10
+		if b > a {
+			fmt.Println("Büyüktür")
+		} else if a == b {
+			fmt.Println("Eşittir")
+		} else {
+			fmt.Println("Küçüktür")
+		}
+	*/
+
+	if foo := 2; foo == 1 {
+		println("bar")
+	} else {
+		println("buz")
+	}
+}
