72ecb3c9f98b982869c257dc8edc8620b0106a69
brtsrm <bsarmis@outlook.com>
2022-08-22 00:48:39 +0300

    break continue kullanımı

---
+++ ders2/break_continue_goto.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/* i := 0
+
+	for {
+		if i >  3 {
+			break
+		}
+		fmt.Println("i'nin değeri : ", i)
+		i++
+	}
+	*/
+
+	for i := 0; i < 7; i++ {
+		if i == 3 {
+			continue
+		} else if i == 5 {
+			break
+		}
+		fmt.Println("Çıktı : ", i)
+	}
+}
