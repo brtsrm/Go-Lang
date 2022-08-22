4c9e4bb58374fcd51a9f0f6a08b789ca8afc177d
brtsrm <bsarmis@outlook.com>
2022-08-22 20:54:54 +0300

    for dongusu

---
+++ ders3/for.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 0; i <= 100; i++ {
+		fmt.Println(i)
+	}
+	/*
+		Sonsuz döngüye girer
+		for i := 0; i < 3;{}
+		for i := 0; ; i++{}
+		for {}
+
+	*/
+}
